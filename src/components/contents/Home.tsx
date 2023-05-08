822c3c9d9a5108f9093e327e90f7f96ecdf316ee
Jerome Jalandoon <jbjalandoon>
2023-05-09 00:26:58 +0800

    text wider

--- src/components/contents/Home.tsx
+++ src/components/contents/Home.tsx
@@ -3,7 +3,7 @@ import Content from "../Content";
 export default function Home() {
   return (
     <Content id='home'>
-      <div className='flex h-full w-full max-w-4xl'>
+      <div className='flex h-full w-full max-w-4xl tracking-wider'>
         <div className='flex w-full flex-col gap-6'>
           <p className='text-lg font-light'>Hello, my name is</p>
           <h1 className='text-7xl font-bold'>Jerome,</h1>
@@ -13,8 +13,8 @@ export default function Home() {
             Philippines Taguig (2023). I specialize in Fullstack Development.
             I'm currently focused in building my own projects.
           </p>
+          <button className="self-start px-7 font-bold transition-all bg-emerald-300 text-slate-950 hover:bg-transparent duration-1000 hover:text-slate-200 py-4 border-emerald-300 border">Resume</button>
         </div>
-        <div></div>
       </div>
     </Content>
   );
