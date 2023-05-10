b78b00e1da5e4e56327c346fa26d2fc1aff7033a
Jerome Jalandoon <jbjalandoon>
2023-05-10 23:23:04 +0800

    project item

--- src/components/contents/Projects.tsx
+++ src/components/contents/Projects.tsx
@@ -1,5 +1,32 @@
 import Content from "../Content";
+import Project from "../Project";
 
 export default function Projects() {
-  return <Content id='project' title='Some of my works'>Some Projects</Content>;
+  return (
+    <Content id='project' title='Some of my works'>
+      <div className="grid grid-cols-3 gap-7">
+        <Project
+          title='My Portfolio'
+          description='Dolor cillum laboris duis adipisicing laborum est. In cillum quis in esse occaecat ea excepteur aliquip reprehenderit velit eiusmod culpa. Exercitation incididunt ea do mollit veniam velit minim do laborum. Laborum excepteur laboris irure laborum exercitation. Tempor nisi commodo exercitation deserunt ullamco ad mollit.'
+          technologies={["NextJS", "Prisma", "Tailwind", "React"]}
+          link='#link'
+          github='#link'
+        />
+        <Project
+          title='My Portfolio'
+          description='Dolor cillum laboris duis adipisicing laborum est. In cillum quis in esse occaecat ea excepteur aliquip reprehenderit velit eiusmod culpa. Exercitation incididunt ea do mollit veniam velit minim do laborum. Laborum excepteur laboris irure laborum exercitation. Tempor nisi commodo exercitation deserunt ullamco ad mollit.'
+          technologies={["NextJS", "Prisma", "Tailwind", "React"]}
+          link='#link'
+          github='#link'
+        />
+        <Project
+          title='My Portfolio'
+          description='Dolor cillum laboris duis adipisicing laborum est. In cillum quis in esse occaecat ea excepteur aliquip reprehenderit velit eiusmod culpa. Exercitation incididunt ea do mollit veniam velit minim do laborum. Laborum excepteur laboris irure laborum exercitation. Tempor nisi commodo exercitation deserunt ullamco ad mollit.'
+          technologies={["NextJS", "Prisma", "Tailwind", "React"]}
+          link='#link'
+          github='#link'
+        />
+      </div>
+    </Content>
+  );
 }
