0bbb6c6ff7058b388082148e55c451aa2fff1d76
Jerome Jalandoon <jbjalandoon>
2023-05-10 23:23:58 +0800

    add dummy project data

---
+++ src/components/Project.tsx
@@ -0,0 +1,46 @@
+import { BiCodeBlock, BiLinkExternal } from "react-icons/bi";
+import { FaGithub } from "react-icons/fa";
+
+export default function Project({
+  title,
+  description,
+  technologies,
+  link,
+  github,
+}: {
+  title: string;
+  description: string;
+  technologies: Array<string>;
+  link: string;
+  github: string;
+}) {
+  return (
+    <div className='flex h-96 w-full flex-col rounded-2xl bg-slate-800 px-8 py-5 gap-7 shadow-xl'>
+      <div className='flex items-center'>
+        <div className='text-5xl font-bold'>
+          <BiCodeBlock />
+        </div>
+        <div className='ml-auto flex gap-3 text-2xl'>
+          <a href={link} target="_blank">
+            <BiLinkExternal />
+          </a>
+          <a href={github} target="_blank">
+            <FaGithub />
+          </a>
+        </div>
+      </div>
+      <h1 className='text-2xl font-bold'>{title}</h1>
+      <p className='mb-3 overflow-hidden text-sm font-extralight line-clamp-3'>
+        {description}
+      </p>
+      <button className='w-44 self-center border border-emerald-300 py-2 font-bold transition-colors duration-500 hover:bg-emerald-300 hover:text-slate-900'>
+        See More
+      </button>
+      <div className='mt-auto flex gap-4 self-center'>
+        {technologies.map((e) => (
+          <span className='text-xs font-extralight'>{e}</span>
+        ))}
+      </div>
+    </div>
+  );
+}
