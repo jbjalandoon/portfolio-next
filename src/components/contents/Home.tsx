5b43be08406e385c10cf50887e4a43002d9a66b0
Jerome Jalandoon <jbjalandoon>
2023-05-13 19:48:23 +0800

    home responsive

--- src/components/contents/Home.tsx
+++ src/components/contents/Home.tsx
@@ -4,11 +4,11 @@ export default function Home() {
   return (
     <Content id='home' title='Welcome to my Portfolio' className="min-h-screen justify-center">
       <div className='flex h-full w-full max-w-4xl tracking-wider'>
-        <div className='flex w-full flex-col gap-6'>
+        <div className='flex w-full flex-col gap-6 transition-all'>
           <p className='text-lg font-light'>Hello, my name is</p>
-          <h1 className='text-7xl font-bold'>Jerome,</h1>
-          <h1 className='text-6xl'>I'm a Web Developer.</h1>
-          <p className='w-4/6 text-lg font-extralight'>
+          <h1 className='md:text-7xl text-5xl font-bold'>Jerome,</h1>
+          <h1 className='md:text-6xl text-4xl'>I'm a Web Developer.</h1>
+          <p className='md:w-4/6 w-full md:text-lg text-base font-extralight'>
             I'm a graduating student in Polytechnic University of the
             Philippines Taguig (2023). I specialize in Fullstack Development.
             I'm currently focused in building my own projects.
