0809d7ebc86efa4b8aa47649aca1f0c03d13d8d8
Jerome Jalandoon <jbjalandoon>
2023-05-13 19:55:40 +0800

    responsive about

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -13,7 +13,7 @@ export default function Content({
 }) {
   return (
     <section
-      className={`flex h-auto w-full max-w-6xl flex-col gap-10 px-5 pt-20 transition-all md:px-20 ${className}`}
+      className={`flex h-auto w-full max-w-6xl flex-col md:gap-10 gap-3 px-5 pt-20 transition-all md:px-20 ${className}`}
       id={id}>
       <div className='mt-5 flex w-full items-center justify-center whitespace-nowrap text-center text-3xl font-semibold before:relative before:-z-50 before:mr-5 before:h-0.5 before:w-full before:bg-slate-600 after:relative after:-z-50 after:ml-5 after:h-0.5 after:w-full after:bg-slate-600'>
         {title}
--- src/components/contents/About.tsx
+++ src/components/contents/About.tsx
@@ -8,9 +8,9 @@ import { TbBrandNextjs } from "react-icons/tb";
 export default function About() {
   return (
     <Content id='about' title='About Me' className='min-h-screen p-20'>
-      <div className='flex flex-col justify-center gap-10'>
-        <div className='grid h-full w-full grid-cols-2 tracking-wider'>
-          <div className='flex flex-col gap-5 px-4 pt-4'>
+      <div className='flex flex-col justify-center gap-0 md:gap-10'>
+        <div className='grid h-full w-full md:grid-cols-2 grid-cols-1 tracking-wider'>
+          <div className='flex flex-col gap-5 px-4 pt-4 text-md text-light'>
             <p>
               Hello, My name is Jerome and I enjoy web development. The interest
               in web development started when I was a graduating student at PUP
@@ -29,13 +29,13 @@ export default function About() {
               contact me.
             </p>
           </div>
-          <div className='flex w-full items-start px-14 pt-14 opacity-75'>
+          <div className='md:flex w-full items-start px-14 pt-14 opacity-75 hidden'>
             <AboutSVG />
           </div>
         </div>
         <div className='flex w-full flex-col items-center justify-center gap-8 mt-3'>
           <h3 className='text-xl font-bold'>SKILLS</h3>
-          <div className='flex flex-wrap gap-10'>
+          <div className='flex flex-wrap gap-10 justify-center'>
             <Stack Svg={FaHtml5} name='HTML' />
             <Stack Svg={FaCss3Alt} name='CSS' />
             <Stack Svg={SiJavascript} name='Javascript' />
